05fe17e102199a3e96d3ea1e28bf7b68ab2d8985
Adilbaeva Meruert <adilbayevameruert@gmail.com>
2024-01-11 11:00:08 +0600

    list of real time fires

---
+++ src/app/fire-rt-incident-catologue/fire-rt-incident-catologue.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-fire-rt-incident-catologue',
+  templateUrl: './fire-rt-incident-catologue.component.html',
+  styleUrls: ['./fire-rt-incident-catologue.component.scss']
+})
+export class FireRtIncidentCatologueComponent implements OnInit {
+
+  constructor(private api: HttpClient, public modalService: NgbModal,  private router: Router, ) { }
+  years = [
+    { value: 2020, label: '2020' },
+    { value: 2021, label: '2021' },
+    { value: 2022, label: '2022' },
+    { value: 2023, label: '2023' },
+    { value: 2024, label: '2024' }
+  ];
+  months = [
+    { value: 1, label: 'Январь' },
+    { value: 2, label: 'Февраль' },
+    { value: 3, label: 'Март' },
+    { value: 4, label: 'Апрель' },
+    { value: 5, label: 'Май' },
+    { value: 6, label: 'Июнь' },
+    { value: 7, label: 'Июль' },
+    { value: 8, label: 'Август' },
+    { value: 9, label: 'Сентябрь' },
+    { value: 10, label: 'Октябрь' },
+    { value: 11, label: 'Ноябрь' },
+    { value: 12, label: 'Декабрь' }
+  ];
+  yearId: any = null;
+  monthId: any = null;
+  listFires: any = null;
+  currentFire: any = false;
+  ngOnInit() {
+  }
+
+  findFire() {
+    console.log(this.yearId);
+    console.log(this.monthId);
+    this.getFires(this.yearId, this.monthId).subscribe((resp: any) => {
+        this.listFires = resp;
+      }
+    );
+    console.log(this.listFires);
+  }
+
+  getFires(year, month) {
+    return this.api.get(`/internal/api/data/RTData/search?year=${year}&month=${month}`);
+  }
+
+  get listIsEmpty() {
+    return this.listFires === false || this.listFires === null || Object.keys(this.listFires).length === 0;
+  }
+  selectEvent(item: any) {
+    this.currentFire = item;
+    console.log(this.currentFire);
+  }
+
+  addFire() {
+  }
+
+  changeFire() {
+    this.router.navigate(['fire/real-time/catalogue/form'], {
+      queryParams: {
+        id: this.currentFire.id,
+      },
+      queryParamsHandling: 'merge'
+    });
+  }
+}
