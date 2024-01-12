1d54efa28a962b00c9f6f802810f54dec6aabf19
Adilbaeva Meruert <adilbayevameruert@gmail.com>
2024-01-12 10:42:45 +0600

    added economic damage report

---
+++ src/app/fire-real-time-economic-damage-report/fire-real-time-economic-damage-report-form/fire-real-time-economic-damage-report-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fire-real-time-economic-damage-report-form',
+  templateUrl: './fire-real-time-economic-damage-report-form.component.html',
+  styleUrls: ['./fire-real-time-economic-damage-report-form.component.css']
+})
+export class FireRealTimeEconomicDamageReportFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/service/real-time-report.service.ts
+++ src/app/service/real-time-report.service.ts
@@ -15,4 +15,18 @@ export class RealTimeReportService {
   getRTReportByRTDataId(id) {
     return this.http.get(`/internal/report/get-all/fire-real-time/by-rt-data-id?rtDataId=${id}`).toPromise();
   }
+
+  getRTReportSearch(year, month) {
+    return this.http.get(`/internal/api/data/RTData/search?year=${year}&month=${month}`).toPromise();
+  }
+
+  getRTReportPdf(requirementId, type = 'pdf', language =   'ru') {
+    return this.http.get(`/internal/report/fire-real-time-overall?reportId=${requirementId}&lang=${language}&type=${type}`,
+      {responseType: 'blob'}).toPromise();
+  }
+
+  createNewReport(data) {
+    return this.http.post(`/internal/report/create/fire-real-time`, data).toPromise();
+  }
 }
+
