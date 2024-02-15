e535e77a1afcf204b48e3c2e972cffbe15e5c88e
deadpearl <adilbayevameruert@gmail.com>
2024-02-15 23:12:56 +0600

    small changes user profile

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -32,4 +32,14 @@ export class UserProfileComponent implements OnInit {
         console.log('Modal dismissed with result:', error);
       });
   }
+
+  formatDate(inputDate) {
+    const date = new Date(inputDate);
+
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');  // Добавляем 1, так как месяцы в JavaScript начинаются с 0
+    const day = date.getDate().toString().padStart(2, '0');
+
+    return `${year}-${month}-${day}`;
+  }
 }
