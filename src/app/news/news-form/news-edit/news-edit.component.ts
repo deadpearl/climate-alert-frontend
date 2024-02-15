f278bdf80edcabdbaed53d059e7922bc6fb4309f
deadpearl <adilbayevameruert@gmail.com>
2024-02-15 23:15:01 +0600

    create and update functional

--- src/app/news/news-form/news-edit/news-edit.component.ts
+++ src/app/news/news-form/news-edit/news-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, ParamMap, Router} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
+import {NewsService} from "../../../service/news.service";
 
 @Component({
   selector: 'app-news-edit',
@@ -7,9 +10,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsEditComponent implements OnInit {
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private api: HttpClient, private router: Router,
+              private newsService: NewsService) { }
 
+  createAction = false;
+  formData: any = {
+     title: '',
+     subtitle: '',
+     text: '',
+     imageUrl: ''
+  };
+  newsId: any;
   ngOnInit() {
+    this.activatedRoute.queryParamMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.get('create') === 'true') {
+        this.createAction = true;
+      } else {
+        this.createAction = false;
+        this.newsId = +paramMap.get('newsId');
+        this.getFormData(this.newsId);
+      }
+    });
   }
+  saveForm() {
+
+    console.log(this.formData)
+    if (this.createAction === true) {
+        this.newsService.createNews(this.formData).then(resp => {
+          console.log('Success create', resp);
+          this.router.navigate(['admin/news/form']);
+        })
+    }
+    else {
+        this.newsService.updateNews(this.newsId, this.formData).then(resp => {
+          console.log('Success update', resp);
+          this.router.navigate(['admin/news/form']);
+        })
+    }
+
+  }
+
+  back(){
+    this.router.navigate(['admin/news/form']);
+  }
+
+  getFormData(id: any) {
+    this.newsService.getOne(id).then(resp => {
+      this.formData = resp;
+      console.log(this.formData);
+    });
+  }
+
 
 }
--- src/app/service/news.service.ts
+++ src/app/service/news.service.ts
@@ -12,5 +12,16 @@ export class NewsService {
     return this.http.get('/internal/api/news').toPromise();
   }
 
+  public getOne(id) {
+    return this.http.get('/internal/api/news/'+id).toPromise();
+  }
+
+  public createNews(news) {
+    return this.http.post('/internal/api/news', news).toPromise();
+  }
+
+  public updateNews(id, news) {
+    return this.http.put('/internal/api/news/' + id, news).toPromise();
+  }
 
 }
