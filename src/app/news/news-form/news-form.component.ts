f18a32e5639256009794eea49b8e39d7d5041269
deadpearl <adilbayevameruert@gmail.com>
2024-02-15 23:15:35 +0600

    news form design changes

--- src/app/news/news-form/news-form.component.ts
+++ src/app/news/news-form/news-form.component.ts
@@ -27,18 +27,29 @@ export class NewsFormComponent implements OnInit {
     });
   }
 
-  goToReadOnly() {
-    console.log('gotoreadonly');
-    this.router.navigate(['fire/report/real-time/form'], {
+  deleteNews() {
+
+  }
+
+  goToEdit(item: any) {
+    this.router.navigate(['admin/news/form/edit'], {
       queryParams: {
-        reportId: 2,
-        readonly: true,
+        newsId: item.id,
+        create: false,
       },
       queryParamsHandling: 'merge'
     });
   }
 
-  deleteNews() {
+  formatDate(inputDate) {
+    const date = new Date(inputDate);
 
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');  // Добавляем 1, так как месяцы в JavaScript начинаются с 0
+    const day = date.getDate().toString().padStart(2, '0');
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+
+    return `${year}-${month}-${day} ${hours}:${minutes}`;
   }
 }
