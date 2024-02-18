a819ed0c3599db9871dc34666b55b800045183ef
deadpearl <adilbayevameruert@gmail.com>
2024-02-18 22:24:28 +0600

    added get notifications

--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../../service/auth.service';
 import {Router} from '@angular/router';
+import {NotificationService} from '../../service/notification.service';
 
 @Component({
   selector: 'app-header',
@@ -10,13 +11,28 @@ import {Router} from '@angular/router';
 export class HeaderComponent implements OnInit {
 
   currentUser: any;
+  notificationList: any = null;
+  isNotificationDivVisible = false;
   constructor(public authService: AuthService,
-              private router: Router) {
+              private router: Router,
+              private notificationService: NotificationService) {
   }
   showOptions: any = false;
   ngOnInit() {
-    this.currentUser = this.authService.getCurrentUser();
-    console.log(this.currentUser);
+     this.authService.getCurrentUser().then(resp => {
+       this.currentUser = resp;
+       console.log(resp);
+       this.getNotifications(this.currentUser.user.email);
+    });
+  }
+
+  async getNotifications(userEmail: string): Promise<void> {
+    try {
+      this.notificationList = await this.notificationService.getAllNotification(userEmail);
+      console.log('Notifications:', this.notificationList);
+    } catch (error) {
+      console.error('Error fetching notifications:', error);
+    }
   }
   logout() {
     this.authService.logout();
@@ -26,5 +42,16 @@ export class HeaderComponent implements OnInit {
   }
   getUrlPosition(url) {
     return this.router.url.includes(url);
+  }
+  isSeen(): boolean {
+    return this.notificationList.some(notification => !notification.seen);
+  }
+  hideNotificationDiv(): void {
+    // Set isNotificationDivVisible to false when the div should be hidden
+    this.isNotificationDivVisible = false;
+  }
+
+  visible() {
+    this.isNotificationDivVisible = !this.isNotificationDivVisible;
   }
 }
