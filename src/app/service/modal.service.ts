0d840abddeda15e64546c08f9eb59de904597b2c
deadpearl <adilbayevameruert@gmail.com>
2024-02-24 22:24:06 +0600

    added region modal open

--- src/app/service/modal.service.ts
+++ src/app/service/modal.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import {ModalComponentComponent} from '../modal-component/modal-component.component';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ModalService {
-  constructor(private modalService: NgbModal) {}
+  constructor(private modalService: NgbModal, private http: HttpClient) {}
   assign = false;
   approve = false;
+  region = false;
 
   openModal(title: string, content: string) {
     const modalRef = this.modalService.open(ModalComponentComponent);
@@ -32,10 +34,32 @@ export class ModalService {
     modalRef.componentInstance.content = content;
     return modalRef.result;
   }
+  selectRegionModal(title: string, content: string): Promise<any> {
+    this.region = true;
+    const modalRef = this.modalService.open(ModalComponentComponent);
+    modalRef.componentInstance.title = title;
+    modalRef.componentInstance.content = content;
+    return modalRef.result;
+  }
   isAssigned() {
     return this.assign;
   }
   isApproving() {
     return this.approve;
+  }
+  isRegionModal() {
+    return this.region;
+  }
+  getRegions(namePrefix, offset) {
+    // tslint:disable-next-line:max-line-length
+    return this.http.get(`http://geodb-free-service.wirefreethought.com/v1/geo/countries/KZ/regions?namePrefix=${namePrefix}&hateoasMode=false&limit=10&offset=${offset}`).toPromise();
+  }
+  getPlace(type, offset, region) {
+    // tslint:disable-next-line:max-line-length
+    return this.http.get(`http://geodb-free-service.wirefreethought.com/v1/geo/countries/KZ/regions/${region}/places?types=${type}&hateoasMode=false&limit=10&offset=${offset}&sort=name`).toPromise();
+  }
+  getAllRegions(offset) {
+    // tslint:disable-next-line:max-line-length
+    return this.http.get(`http://geodb-free-service.wirefreethought.com/v1/geo/countries/KZ/regions?namePrefix=&hateoasMode=false&limit=10&offset=${offset}`).toPromise();
   }
 }
